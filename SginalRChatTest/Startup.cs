077c086a5b4a2477aae972a7c87dda7713d18c9f
Aleks <aleksf11125@outlook.com>
2020-11-02 13:04:12 +0100

    Added email sender

--- SginalRChatTest/Startup.cs
+++ SginalRChatTest/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SginalRChatTest.Hubs;
+using SginalRChatTest.Services;
 
 namespace SginalRChatTest
 {
@@ -20,6 +23,19 @@ namespace SginalRChatTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
             services.AddRazorPages(options =>
             {
                 options.Conventions.AuthorizePage("/Index");
