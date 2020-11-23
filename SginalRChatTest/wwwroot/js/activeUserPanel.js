d21157f7f9a58e91141c16545305764648169104
padii <chudy3221@gmail.com>
2020-11-23 20:53:41 +0100

    Primitive mobile looks for chat sidebar
    (*) Changed behaviour from changing 'left' to changing 'zindex'

--- SginalRChatTest/wwwroot/js/activeUserPanel.js
+++ SginalRChatTest/wwwroot/js/activeUserPanel.js
@@ -1,31 +1,71 @@
 ﻿function changeBox() {
     function widthListener(x) {
         if (x.matches) {
+            //If the max width is lesser than 1182px
             if (document.getElementById("listCheckbox").checked == true) {
-                anime({
-                    targets: '.active-users-panel',
-                    transformX: document.getElementById("activeUsersPanel").offsetWidth * 0.8,
+                //If arrow was clicked
+                var chatBox = anime({
+                    targets: '.typing-messages-box',
+                    opacity: 0,
+                    duration: 100,
+                    easing: 'linear'
+                });
+
+                var userPanel = anime({
+                    targets: '#wholeActiveUsersPanel',
+                    left: '-8px',
+                    zIndex: {
+                        value: 3
+                    },
+                    duration: 100,
+                    easing: 'easeInOutQuad'
+                });
+
+                var userList = anime({
+                    targets: '#usersBox',
+                    opacity: 1,
                     easing: 'easeInOutQuad'
                 });
             }
             else {
-                anime({
-                    targets: '.active-users-panel',
-                    transformX: 0,
+                //If arrow was not clicked
+                var chatBox = anime({
+                    targets: '.typing-messages-box',
+                    opacity: 1,
+                    duration: 100,
+                    easing: 'linear'
+                });
+
+                var userPanel = anime({
+                    targets: '#wholeActiveUsersPanel',
+                    left: '-8px',
+                    zIndex: {
+                        value: 1
+                    },
+                    duration: 100,
+                    easing: 'easeInOutQuad'
+                });
+
+                var userList = anime({
+                    targets: '#usersBox',
+                    opacity: 0,
                     easing: 'easeInOutQuad'
                 });
             }
         } else {
+            //If the max width is bigger than 1182px
             if (document.getElementById("listCheckbox").checked == true) {
+                //If arrow was clicked
                 anime({
-                    targets: '.active-users-panel',
+                    targets: '#wholeActiveUsersPanel',
                     left: '220px',
                     easing: 'easeInOutQuad'
                 });
             }
             else {
+                //If arrow was not clicked
                 anime({
-                    targets: '.active-users-panel',
+                    targets: '#wholeActiveUsersPanel',
                     left: '0px',
                     easing: 'easeInOutQuad'
                 });
