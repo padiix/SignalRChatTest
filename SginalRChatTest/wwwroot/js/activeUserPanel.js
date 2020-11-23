4eb6c9acd9a842d56cee0d1c87d0969c96baf8d6
padii <chudy3221@gmail.com>
2020-11-23 18:20:39 +0100

    Sidebar with active users

---
+++ SginalRChatTest/wwwroot/js/activeUserPanel.js
@@ -0,0 +1,39 @@
+﻿function changeBox() {
+    function widthListener(x) {
+        if (x.matches) {
+            if (document.getElementById("listCheckbox").checked == true) {
+                anime({
+                    targets: '.active-users-panel',
+                    transformX: document.getElementById("activeUsersPanel").offsetWidth * 0.8,
+                    easing: 'easeInOutQuad'
+                });
+            }
+            else {
+                anime({
+                    targets: '.active-users-panel',
+                    transformX: 0,
+                    easing: 'easeInOutQuad'
+                });
+            }
+        } else {
+            if (document.getElementById("listCheckbox").checked == true) {
+                anime({
+                    targets: '.active-users-panel',
+                    left: '220px',
+                    easing: 'easeInOutQuad'
+                });
+            }
+            else {
+                anime({
+                    targets: '.active-users-panel',
+                    left: '0px',
+                    easing: 'easeInOutQuad'
+                });
+            }
+        }
+    }
+
+    var x = window.matchMedia("(max-width: 1182px)");
+    widthListener(x);
+    x.addListener(widthListener);
+}
